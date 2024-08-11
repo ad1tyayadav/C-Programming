71d1e8e644ae320b776ea338d892bf76d549d635
Adii Code <adityaproworks@gmail.com>
2024-08-12 01:58:00 +0530

    for and while loop

---
+++ Control Structer/while-loop/sumofn.c
@@ -0,0 +1,17 @@
+// Sum of firs ten natural numbers
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    int sum = 0;
+    while (i <= 10)
+    {
+        sum += i;
+        i++;
+    };
+    printf("Sum of the first ten natural numbers: %d\n", sum);
+    return 0;
+};
+// Output = 55
